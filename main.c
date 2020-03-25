873eb7a48d9048d30c5e1e3e8bd83005be4c27ab
Jakub Grobelny <jgrobelny1@gmail.com>
2020-03-25 04:11:33 +0100

    Added TODOs

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <unistd.h>
 #include <errno.h>
 #include <string.h>
-#include <netinet/in.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void make_icmp_packet(const struct sockaddr_in* addr, int ttl, void* buf) {
+    // TODO: implement
+    fprintf(stderr, "make_icmp_packet: not implemented!\n");
+    exit(EXIT_FAILURE);
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -15,7 +24,7 @@ int main(int argc, char* argv[]) {
     const char* address = argv[1];
     const pid_t pid = getpid();
 
-    struct sockaddr_in addr;
+    struct sockaddr_in addr = {0};
     if (inet_pton(AF_INET, address, &addr.sin_addr) == 0) {
         fprintf(stderr, "'%s' is not a valid network address!\n", address);
         return EXIT_FAILURE;
@@ -27,7 +36,17 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
 
+    for (int ttl = 1; ttl <= 30; ttl++) {
+        char buffer[IP_MAXPACKET] = {0};
+        make_icmp_packet(&addr, ttl, buffer);
+        // TODO: send three packets
+        // TODO: wait for three packets up to 1 second
+        // TODO: calculate the average time for each router that responded
+        // TODO: if the response came from destination address then abort
+    }
+
     printf("pid: [%d], socket: [%d], address:[%s]\n", pid, socket_fd, address);
 
+    close(socket_fd);
     return 0;
 }
